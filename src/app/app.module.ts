ac5a5b9c7972a8e5b066a9d56cab225d43b72090
ACS-CAIRO\aZaki <azaki@acs-me.com>
2018-02-08 19:35:09 +0200

    Navbar Again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { MainService } from './services/main-service.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router'
 import { HttpModule } from '@angular/http'
 
 import { RecaptchaModule } from 'ng-recaptcha';
@@ -12,20 +12,26 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { CustomerFormComponent } from './customers/customer-form/customer-form.component';
 import { MailSendComponent } from './mail-send/mail-send.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerFormComponent,
-    MailSendComponent
+    MailSendComponent,
+    NavbarComponent , 
+    
   ],
   imports: [
     BrowserModule, 
     FormsModule,
     RecaptchaModule.forRoot(), // Keep in mind the "forRoot"-magic nuances!
     RecaptchaFormsModule, 
-    HttpModule
+    HttpModule ,
+    RouterModule.forRoot([ { 
+      path : 'Customer' ,component: CustomerFormComponent
+    }])
   ],
   providers: [
     MainService
--- src/app/mail-send/mail-send.component.ts
+++ src/app/mail-send/mail-send.component.ts
@@ -13,29 +13,7 @@ export class MailSendComponent {
   constructor(item? : MainService) {  
   }
 
-  serverContent = {
-  user: 'angularTesting2022@gmail',
-  password: '123456789@acs',
-  host: 'smtp.gmail.com',
-  ssl: true,
-  Port: 2525 
-   //587 465
-};
-   message = {
-    text: 'Hey from angular',
-    from: 'angularTesting',
-    to: 'ssss <ssss@send22u.info>',
-    cc: 'abdel_moneim <abdel_moneim122@hotmail.com>',
-    subject: 'Greetings'
-  } ;
 
-  sendMail() {  
-
-     var email = require('emailjs'); 
-     var server = email.server.connect(this.serverContent); 
-     server.send(this.message , function (err, message) { console.log(err || message); });
-
-   
-  }
+  
 
 }
