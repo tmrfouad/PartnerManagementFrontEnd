119edd8066e5788a00861e4f913d26cbf59a31c3
Tamer Fouad Abuhmead <tmrfouad@gmail.com>
2018-02-11 18:52:22 +0200

    some code clean up

--- src/app/customers/customer-form/customer-form.component.ts
+++ src/app/customers/customer-form/customer-form.component.ts
@@ -19,38 +19,6 @@ export class CustomerFormComponent implements OnInit {
 
   ngOnInit() {
   }
-<<<<<<< HEAD
-  
- paymentItem : {} ;
-
- logForm(f) {
-  this.paymentItem = {
-    "delivery_needed": "false",
-    "merchant_id": "28",     
-    "amount_cents": "100",
-    "currency": "EGP",  
-    "shipping_dataId": 1,
-    "items": [],
-    "shipping_data": {
-      "apartment": "803", 
-      "email": f.email, 
-      "floor": "42", 
-      "first_name": f.firstName, 
-      "street": "Ethan Land", 
-      "building": "8028", 
-      "phone_number": f.phoneNumber, 
-      "postal_code": "01898", 
-      "city": "Jaskolskiburgh", 
-      "country": "CR", 
-      "last_name": f.lastName, 
-      "state": "Utah"
-    }
-  }
-  this.orderService.Post(f).subscribe( () => console.log("sucess") );
- 
-  }
-=======
->>>>>>> 0588a12c2550c83f7f7bd4af5d142c526034b1d8
 
 
   logForm(f) {
