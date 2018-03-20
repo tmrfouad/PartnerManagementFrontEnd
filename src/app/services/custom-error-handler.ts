e7e046edc1d22f515f974dc07d3f1504edba7a8f
Tamer Fouad Abuhmead <tmrfouad@gmail.com>
2018-03-20 15:37:07 +0200

    handle messages for 515 and 2601 errors

--- src/app/services/custom-error-handler.ts
+++ src/app/services/custom-error-handler.ts
@@ -11,8 +11,39 @@ export class CutomErrorHandler implements ErrorHandler {
         const errorObjStr = errString.substring(errString.indexOf('{'), errString.lastIndexOf('}') + 1);
         const errorObj = JSON.parse(errorObjStr);
 
+        const errStatus: number = errorObj.status;
+        let errMsg;
+
+        switch (errStatus) {
+            case 0:
+                errMsg = 'Failed to contact server.';
+                break;
+            default:
+                if (errorObj.error.Data) {
+                    const errId = errorObj.error.Data['HelpLink.EvtID'];
+                    console.log(errId);
+                    switch (errId) {
+                        case '515':
+                            errMsg = 'The data you entered is incomplete.';
+                            break;
+                        case '2601':
+                            errMsg = 'The data you entered is repeated.';
+                            break;
+                        default:
+                            errMsg = errorObj.error.Message;
+                            break;
+                    }
+                } else {
+                    errMsg = errorObj.message;
+                }
+                break;
+        }
+
+        console.log(errorObj);
+
         const snakBar = this.injector.get(MatSnackBar);
-        snakBar.open(errorObj.error.Message, 'Error', {
+        snakBar.open(errMsg, 'Error', {
+            verticalPosition: 'bottom',
             duration: 5000,
             panelClass: 'snack-bar-error'
         });
