fd2209d2677e10bc80823c2089ca975b0df94cf3
Tamer Fouad Abuhmead <tmrfouad@gmail.com>
2018-03-27 06:49:07 +0200

    fix design bus in scroll

--- src/app/components/rfq/summary/summary.component.ts
+++ src/app/components/rfq/summary/summary.component.ts
@@ -5,12 +5,12 @@ import { ActionTypeComment } from '../../../models/ActionTypeComment';
   // tslint:disable-next-line:component-selector
   selector: 'summary-info',
   templateUrl: './summary.component.html',
-  styleUrls:  ['./summary.component.css']
+  styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
 
   constructor() {
-   }
+  }
   @Input('actionType') actionType: ActionTypeComment = <ActionTypeComment>{};
   // tslint:disable-next-line:no-output-on-prefix
   @Output() addSummary = new EventEmitter();
@@ -19,44 +19,47 @@ export class SummaryComponent implements OnInit {
     this.actionType.attende = [];
   }
 
+  validate(): boolean {
+    return true;
+  }
   addItem(item: HTMLInputElement) {
     if (item.value) {
       this.actionType.attende.push(item.value);
-      this.addSummary.emit(this.addSummery());
+      this.addSummary.emit({ summary: this.addSummery(), active: this.validate() });
       item.value = '';
     }
   }
 
   changeItem() {
-    this.addSummary.emit(this.addSummery());
+    this.addSummary.emit({ summary: this.addSummery(), active: this.validate() });
   }
 
-addSummery(): string {
-      let Where = ' ', VisitReason = ' ', comment = ' ', Attended = ' ', summery: string ;
-      if (this.actionType.where) {
-       Where = ' Where: ' + this.actionType.where ;
+  addSummery(): string {
+    let Where = ' ', VisitReason = ' ', comment = ' ', Attended = ' ', summery: string;
+    if (this.actionType.where) {
+      Where = ' Where: ' + this.actionType.where;
+    }
+    if (this.actionType.visitReason) {
+      VisitReason = ' Visit Reason: ' + this.actionType.visitReason;
+    }
+    if (this.actionType.comment) {
+      comment = ' Comments: ' + this.actionType.comment;
+    }
+    if (this.actionType.attende.length > 0) {
+      Attended = ' Attended: ';
+      for (const item of this.actionType.attende) {
+        Attended += ' ' + item + ', ';
       }
-      if (this.actionType.visitReason) {
-        VisitReason = ' Visit Reason: ' + this.actionType.visitReason ;
-      }
-      if (this.actionType.comment) {
-        comment = ' Comments: ' + this.actionType.comment;
-      }
-      if (this.actionType.attende.length > 0) {
-        Attended = ' Attended: ';
-        for (const item of this.actionType.attende) {
-          Attended += ' ' + item + ', ';
-          }
-      }
-      summery = Where + VisitReason + comment + Attended;
-      summery = summery.substring(0, summery.length - 2 );
-      return summery.trim();
+    }
+    summery = Where + VisitReason + comment + Attended;
+    summery = summery.substring(0, summery.length - 2);
+    return summery.trim();
   }
 
   removeItem(item) {
     const index = this.actionType.attende.indexOf(item);
     this.actionType.attende.splice(index, 1);
-    this.addSummary.emit(this.addSummery());
+    this.addSummary.emit({ summary: this.addSummery(), active: this.validate() });
   }
 
 
