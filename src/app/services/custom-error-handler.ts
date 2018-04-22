40db5a268023b17a6a247451fb77f59ae35c88cd
ACS-CAIRO\aZaki <abdel_moneim122@hotmail.com>
2018-04-22 11:02:39 +0200

    Fixing SnakBar Error style to be able to display error any time without any issue

--- src/app/services/custom-error-handler.ts
+++ src/app/services/custom-error-handler.ts
@@ -1,95 +1,100 @@
-import { ErrorHandler, Injectable, Injector, style } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, style, NgZone } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class CutomErrorHandler implements ErrorHandler {
-    constructor(private injector: Injector) {
+    constructor(private injector: Injector, private zone: NgZone) {
 
     }
     handleError(error): void {
         try {
-        const errString: string = error.toString();
-        const braces = Object.entries(errString).filter(c => c['1'] === '{' || c['1'] === '}');
+            const errString: string = error.toString();
+            const braces = Object.entries(errString).filter(c => c['1'] === '{' || c['1'] === '}');
 
-        const errObjArray = [];
+            const errObjArray = [];
 
-        if (braces.length === 0) {
-            const errStringNoTrace = errString.split(' at ')[0].replace('\n', ' ');
-            const errs = errStringNoTrace.split('Error: ');
-            let errorsConc = '';
-            for (let i = 0; i < errs.length; i++) {
-                const err = errs[i].trim();
-                if (!err) { continue; }
-                if (err === '') { continue; }
-                errorsConc += (errorsConc === '' ||
-                    errorsConc.substring(errorsConc.length - 1, errorsConc.length) === ':' ? '' : '; ') + err;
+            if (braces.length === 0) {
+                const errStringNoTrace = errString.split(' at ')[0].replace('\n', ' ');
+                const errs = errStringNoTrace.split('Error: ');
+                let errorsConc = '';
+                for (let i = 0; i < errs.length; i++) {
+                    const err = errs[i].trim();
+                    if (!err) { continue; }
+                    if (err === '') { continue; }
+                    errorsConc += (errorsConc === '' ||
+                        errorsConc.substring(errorsConc.length - 1, errorsConc.length) === ':' ? '' : '; ') + err;
+                }
+
+                const errorObjStr = `{ "status": -1, "message": "${errorsConc}" }`;
+                errObjArray.push(JSON.parse(errorObjStr));
+            } else {
+                // tslint:disable-next-line:radix
+                let openIndx = parseInt(braces[0]['0']);
+                let closeIndx = -1;
+                let openCnt = 0;
+                braces.forEach(b => {
+                    if (openCnt === 0) {
+                        // tslint:disable-next-line:radix
+                        openIndx = parseInt(b['0']);
+                    }
+                    if (b['1'] === '{') {
+                        openCnt++;
+                    }
+                    if (b['1'] === '}') {
+                        openCnt--;
+                    }
+                    if (openCnt === 0) {
+                        // tslint:disable-next-line:radix
+                        closeIndx = parseInt(b['0']);
+                        const errStr = errString.substring(openIndx, closeIndx + 1);
+                        errObjArray.push(JSON.parse(errStr));
+                    }
+                });
             }
 
-            const errorObjStr = `{ "status": -1, "message": "${errorsConc}" }`;
-            errObjArray.push(JSON.parse(errorObjStr));
-        } else {
-            // tslint:disable-next-line:radix
-            let openIndx = parseInt(braces[0]['0']);
-            let closeIndx = -1;
-            let openCnt = 0;
-            braces.forEach(b => {
-                if (openCnt === 0) {
-                    // tslint:disable-next-line:radix
-                    openIndx = parseInt(b['0']);
-                }
-                if (b['1'] === '{') {
-                    openCnt++;
-                }
-                if (b['1'] === '}') {
-                    openCnt--;
-                }
-                if (openCnt === 0) {
-                    // tslint:disable-next-line:radix
-                    closeIndx = parseInt(b['0']);
-                    const errStr = errString.substring(openIndx, closeIndx + 1);
-                    errObjArray.push(JSON.parse(errStr));
-                }
+            const errorObj = errObjArray[0];
+
+            const errStatus: number = errorObj.status;
+            let errMsg;
+
+            switch (errStatus) {
+                case 0:
+                    errMsg = 'Failed to contact server.';
+                    break;
+                default:
+                    if (errorObj.error && errorObj.error.Data) {
+                        const errId = errorObj.error.Data['HelpLink.EvtID'];
+                        switch (errId) {
+                            case '515':
+                                errMsg = 'The data you entered is incomplete.';
+                                break;
+                            case '2601':
+                                errMsg = 'The data you entered is repeated.';
+                                break;
+                            default:
+                                errMsg = errorObj.error.Message;
+                                break;
+                        }
+                    } else {
+                        errMsg = errorObj.message;
+                    }
+                    break;
+            }
+
+            const snakBar = this.injector.get(MatSnackBar);
+            if (this.zone) {
+                this.zone.run(() => {
+                    snakBar.open(errMsg, 'Error', {
+                        verticalPosition: 'bottom',
+                        duration: 5000,
+                        panelClass: 'snack-bar-error'
+                    });
             });
         }
 
-        const errorObj = errObjArray[0];
-
-        const errStatus: number = errorObj.status;
-        let errMsg;
-
-        switch (errStatus) {
-            case 0:
-                errMsg = 'Failed to contact server.';
-                break;
-            default:
-                if (errorObj.error && errorObj.error.Data) {
-                    const errId = errorObj.error.Data['HelpLink.EvtID'];
-                    switch (errId) {
-                        case '515':
-                            errMsg = 'The data you entered is incomplete.';
-                            break;
-                        case '2601':
-                            errMsg = 'The data you entered is repeated.';
-                            break;
-                        default:
-                            errMsg = errorObj.error.Message;
-                            break;
-                    }
-                } else {
-                    errMsg = errorObj.message;
-                }
-                break;
+        } catch {
+            throw error;
         }
-
-        const snakBar = this.injector.get(MatSnackBar);
-        snakBar.open(errMsg, 'Error', {
-            verticalPosition: 'bottom',
-            duration: 5000,
-            panelClass: 'snack-bar-error'
-        });
-    } catch {
-        throw error;
-    }
         console.log('Error log :', error);
         // throw error;
     }
